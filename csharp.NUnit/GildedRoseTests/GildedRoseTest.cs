fb93d40bfc4c2eec924e6bd0f9ac7951d0ebf875
Davi Garms <davi.garms@spektrix.com>
2024-08-28 11:57:06 +0100

    Add Aged Brie tests

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -38,4 +38,30 @@ public class GildedRoseShould
         Assert.That(items[1].Quality, Is.EqualTo(0));
 
     }
+
+    [Test]
+    public void UpdateQuality_by_increasing_brie_quality_by_1_while_within_sellin_date()
+    {
+        var items = new List<Item>
+        {
+            new Item { Name = "Aged Brie", SellIn = 1, Quality = 2 }
+        };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(3));
+    }
+    
+    [Test]
+    public void UpdateQuality_by_increasing_brie_quality_by_2_after_sellin_date()
+    {
+        var items = new List<Item>
+        {
+            new Item { Name = "Aged Brie", SellIn = 0, Quality = 2 },
+            new Item { Name = "Aged Brie", SellIn = 0, Quality = 3 }
+        };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(4));
+        Assert.That(items[1].Quality, Is.EqualTo(5));
+    }
 }
