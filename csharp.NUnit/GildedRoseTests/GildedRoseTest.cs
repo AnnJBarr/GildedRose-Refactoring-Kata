b97e7904b7c4c7b8e3b54b353013aed0c6597a66
Ann Barr <ann.barr@spektrix.com>
2024-08-28 11:39:15 +0100

    add testing for item beyond sell by date

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,13 @@ public class GildedRoseShould
         app.UpdateQuality();
         Assert.That(items[0].Quality, Is.EqualTo(1));
     }
+    
+    [Test]
+    public void UpdateQuality_by_reducing_non_special_item_quality_by_2_after_sellin_date()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
 }
