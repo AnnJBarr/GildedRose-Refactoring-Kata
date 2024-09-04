cad4c75bf78c29506d2d5e0a5df11f3e98522bad
David Hobson <david.hobson@spektrix.com>
2024-09-04 12:05:25 +0100

    update tests to improve sellin coverage

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,16 +7,29 @@ namespace GildedRoseTests;
 public class GildedRoseShould
 {
     [Test]
-    public void UpdateQuality_by_reducing_non_special_item_quality_by_1_while_within_sellin_date()
+    public void UpdateQuality_by_reducing_non_legendary_item_quality_by_1_while_within_sellin_date()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 2 } };
         var app = new GildedRose(items);
+        
         app.UpdateQuality();
+        
         Assert.That(items[0].Quality, Is.EqualTo(1));
     }
+
+    [Test]
+    public void UpdateQuality_by_reducing_non_legendary_item_sellin_by_1_while_within_sellin_date()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 2 } };
+        var app = new GildedRose(items);
+        
+        app.UpdateQuality();
+        
+        Assert.That(items[0].SellIn, Is.EqualTo(0));
+    }    
     
     [Test]
-    public void UpdateQuality_by_reducing_non_special_item_quality_by_2_after_sellin_date()
+    public void UpdateQuality_by_reducing_non_legendary_item_quality_by_2_after_sellin_date()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
         var app = new GildedRose(items);
