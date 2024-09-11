6b721d6463e4d8ce3dcedb0ac98d905633f89d10
Davi Garms <davi.garms@spektrix.com>
2024-09-11 11:49:23 +0100

    Extract QualityNotMax method

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -27,7 +27,7 @@ public class GildedRose
             }
             else
             {
-                if (item.Quality < 50)
+                if (QualityNotMax(item))
                 {
                     IncreaseQuality(item);
 
@@ -35,18 +35,12 @@ public class GildedRose
                     {
                         if (item.SellIn < 11)
                         {
-                            if (item.Quality < 50)
-                            {
-                                IncreaseQuality(item);
-                            }
+                            if (QualityNotMax(item)) IncreaseQuality(item);
                         }
 
                         if (item.SellIn < 6)
                         {
-                            if (item.Quality < 50)
-                            {
-                                IncreaseQuality(item);
-                            }
+                            if (QualityNotMax(item)) IncreaseQuality(item);
                         }
                     }
                 }
@@ -78,14 +72,13 @@ public class GildedRose
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        IncreaseQuality(item);
-                    }
+                    if (QualityNotMax(item)) IncreaseQuality(item);
                 }
             }
         }
     }
+
+    private static bool QualityNotMax(Item item) => item.Quality < 50;
 
     private static void IncreaseQuality(Item item) => item.Quality += 1;
 }
