8b0e205d61220b398899059955f3971f89613844
David Hobson <david.hobson@spektrix.com>
2024-09-11 11:57:35 +0100

    refactor item quality adjustment

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -21,7 +21,7 @@ public class GildedRose
                 {
                     if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        item.Quality = item.Quality - 1;
+                        ChangeQuality(item,-1);
                     }
                 }
             }
@@ -29,18 +29,18 @@ public class GildedRose
             {
                 if (QualityNotMax(item))
                 {
-                    IncreaseQuality(item);
+                    ChangeQuality(item,1);
 
                     if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
                         if (item.SellIn < 11)
                         {
-                            if (QualityNotMax(item)) IncreaseQuality(item);
+                            if (QualityNotMax(item)) ChangeQuality(item,1);
                         }
 
                         if (item.SellIn < 6)
                         {
-                            if (QualityNotMax(item)) IncreaseQuality(item);
+                            if (QualityNotMax(item)) ChangeQuality(item,1);
                         }
                     }
                 }
@@ -61,7 +61,7 @@ public class GildedRose
                         {
                             if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                item.Quality = item.Quality - 1;
+                                ChangeQuality(item,-1);
                             }
                         }
                     }
@@ -72,7 +72,7 @@ public class GildedRose
                 }
                 else
                 {
-                    if (QualityNotMax(item)) IncreaseQuality(item);
+                    if (QualityNotMax(item)) ChangeQuality(item,1);
                 }
             }
         }
@@ -80,5 +80,5 @@ public class GildedRose
 
     private static bool QualityNotMax(Item item) => item.Quality < 50;
 
-    private static void IncreaseQuality(Item item) => item.Quality += 1;
+    private static void ChangeQuality(Item item, int amount) => item.Quality += amount;
 }
