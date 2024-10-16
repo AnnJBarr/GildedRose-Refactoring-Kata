d0b07656ba6a622bfcb46fcfb48d34ec403ec439
David Hobson <david.hobson@spektrix.com>
2024-10-16 11:47:49 +0100

    added brie handler and test

---
+++ csharp.NUnit/GildedRose/BrieItemHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace GildedRoseKata;
+
+public class BrieItemHandler : Handler
+{
+    public override void UpdateItem(Item item)
+    {
+        if (item.Name != "Aged Brie")
+        {
+            NextHandler.UpdateItem(item);
+        }
+    }
+}
--- csharp.NUnit/GildedRoseTests/BrieItemHandlerShould.cs
+++ csharp.NUnit/GildedRoseTests/BrieItemHandlerShould.cs
@@ -7,24 +7,17 @@ namespace GildedRoseTests;
 [TestFixture]
 public class BrieItemHandlerShould
 {
-    [Test]
-    public void Only_handle_brie()
+    [TestCase("Random Item", 1)]
+    [TestCase("Aged Brie",0)]
+    public void Only_handle_brie(string name,int numberOfCalls)
     {
-        var item = new Item { Name = "Random Item", SellIn = 2, Quality = 2 };
+        var item = new Item { Name = name, SellIn = 2, Quality = 2 };
         var nextHandler = A.Fake<Handler>();
         var brieItemHandler = new BrieItemHandler();
 
         brieItemHandler.SetNextHandler(nextHandler);
         brieItemHandler.UpdateItem(item);
 
-        A.CallTo(() => nextHandler.UpdateItem(item)).MustHaveHappened();
-    }
-}
-
-public class BrieItemHandler : Handler
-{
-    public override void UpdateItem(Item item)
-    {
-        NextHandler.UpdateItem(item);
+        A.CallTo(() => nextHandler.UpdateItem(item)).MustHaveHappenedANumberOfTimesMatching(i => i == numberOfCalls);
     }
 }
