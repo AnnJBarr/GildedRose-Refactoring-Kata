f96252ec5a7613969bfc4d658c70a499c0a5706e
Davi Garms <davi.garms@spektrix.com>
2024-10-16 11:33:21 +0100

    Implement BrieItemHandler UpdateItem method

---
+++ csharp.NUnit/GildedRoseTests/BrieItemHandlerShould.cs
@@ -0,0 +1,30 @@
+﻿using FakeItEasy;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+[TestFixture]
+public class BrieItemHandlerShould
+{
+    [Test]
+    public void Only_handle_brie()
+    {
+        var item = new Item { Name = "Random Item", SellIn = 2, Quality = 2 };
+        var nextHandler = A.Fake<Handler>();
+        var brieItemHandler = new BrieItemHandler();
+
+        brieItemHandler.SetNextHandler(nextHandler);
+        brieItemHandler.UpdateItem(item);
+
+        A.CallTo(() => nextHandler.UpdateItem(item)).MustHaveHappened();
+    }
+}
+
+public class BrieItemHandler : Handler
+{
+    public override void UpdateItem(Item item)
+    {
+        NextHandler.UpdateItem(item);
+    }
+}
